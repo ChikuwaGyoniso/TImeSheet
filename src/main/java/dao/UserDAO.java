3c20cadabcad60ab9a14f960f3f6c18166a3af4c
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-08-31 21:04:23 +0900

    appuserテーブルを担当するDAOの作成

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Login;
+import model.User;
+
+public class UserDAO {
+  private final String JDBC_URL = "jdbc:mysql://localhost:3306/appuser?characterEncording=UTF-8";
+  private final String DB_USER = "Sampleuser";
+  private final String DB_PASS = "chikuwanoowari458";
+  private final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+  public User findByLogin(Login login) {
+	  User user = null;
+
+	  try(Connection conn = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS)){
+		  Class.forName(DB_DRIVER);
+
+		  String sql = "SELECT USER_ID, PASS, MAIL, NAME FROM ACCOUNT WHERE USER_ID = ? AND PASS = ?";
+
+		  PreparedStatement pstmt = conn.prepareStatement(sql);
+		  pstmt.setString(1, login.getUserId());
+		  pstmt.setString(2, login.getPass());
+
+		  ResultSet rs = pstmt.executeQuery();
+
+		  if(rs.next()) {
+			  String userId = rs.getString("USER_ID");
+			  String pass = rs.getString("PASS");
+			  String mail = rs.getString("MAIL");
+			  String name = rs.getString("NAME");
+			  user = new User(userId,pass,mail,name);
+		  }
+	  }catch(SQLException e) {
+		  e.printStackTrace();
+		  return null;
+	  }catch(ClassNotFoundException e) {
+		  e.printStackTrace();
+		  return null;
+	  }
+	  return user;
+  }
+}
