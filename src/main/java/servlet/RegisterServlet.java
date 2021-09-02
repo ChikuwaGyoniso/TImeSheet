c7385d8d23837622230fd3ea3d53bdd12d05bd25
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-02 21:42:28 +0900

    ユーザー登録に関するリクエストを処理するコントローラ

---
+++ src/main/java/servlet/RegisterServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.RegisterLogic;
+import model.User;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/register.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String pass = request.getParameter("pass");
+		String mail = request.getParameter("mail");
+		String name = request.getParameter("name");
+
+		User user = new User(userId, pass, mail, name);
+		RegisterLogic rl = new RegisterLogic();
+		rl.execute(user);
+	}
+
+}
