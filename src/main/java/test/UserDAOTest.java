f8b31217e7c67aaed085d3157f09d028b3e29fbb
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-02 22:06:38 +0900

    Registerメソッドをテストするメソッドの作成

--- src/main/java/test/UserDAOTest.java
+++ src/main/java/test/UserDAOTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import dao.RegisterTestDAO;
 import dao.UserDAO;
 import model.Login;
 import model.User;
@@ -8,6 +9,7 @@ public class UserDAOTest {
 	public static void main(String[] arga) {
 		testFindByLogin1();//ユーザーが見つかる場合
 		testFindByLogin2();//ユーザーが見つからない場合
+		testRegister();//ユーザー登録テスト
 	}
 
 	public static void testFindByLogin1() {
@@ -32,6 +34,26 @@ public class UserDAOTest {
 			System.out.println("testFindByLogin2:成功しました");
 		} else {
 			System.out.println("testFindByLogin2:失敗しました");
+		}
+	}
+
+	public static void testRegister() {
+		User user = new User("testUser", "123456", "sample@email", "テストテスト");
+		Login login = new Login("testuser", "123456");
+		UserDAO dao = new UserDAO();
+		RegisterTestDAO testdao = new RegisterTestDAO();
+		//テストユーザーを仮登録
+		dao.RegisterUser(user);
+		//テストユーザーで仮ログイン
+		User result = dao.findByLogin(login);
+		if (result != null && result.getUserId().equals("testUser") && result.getPass().equals("123456")
+				&& result.getMail().equals("sample@email") && result.getName().equals("テストテスト")) {
+			System.out.println("testRegister:成功しました");
+			//仮登録したユーザーを削除
+			testdao.DeleteUser(user);
+		} else {
+			System.out.println("testRegister:失敗しました");
+			testdao.DeleteUser(user);
 		}
 	}
 }
