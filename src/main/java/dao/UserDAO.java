209115aac448a3d8870b5778ef98d82a62d85661
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-02 20:53:29 +0900

    ユーザー登録用メソッドの追加

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -10,39 +10,67 @@ import model.Login;
 import model.User;
 
 public class UserDAO {
-  private final String JDBC_URL = "jdbc:mysql://localhost:3306/sampleappdb?characterEncording=UTF-8";
-  private final String DB_USER = "Sampleuser";
-  private final String DB_PASS = "chikuwanoowari458";
-  private final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private final String JDBC_URL = "jdbc:mysql://localhost:3306/sampleappdb?characterEncording=UTF-8";
+	private final String DB_USER = "Sampleuser";
+	private final String DB_PASS = "chikuwanoowari458";
+	private final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 
-  public User findByLogin(Login login) {
-	  User user = null;
+	public User findByLogin(Login login) {
+		User user = null;
 
-	  try(Connection conn = DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS)){
-		  Class.forName(DB_DRIVER);
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+			Class.forName(DB_DRIVER);
 
-		  String sql = "SELECT USER_ID, PASS, MAIL, NAME FROM APPUSER WHERE USER_ID = ? AND PASS = ?";
+			String sql = "SELECT USER_ID, PASS, MAIL, NAME FROM APPUSER WHERE USER_ID = ? AND PASS = ?";
 
-		  PreparedStatement pstmt = conn.prepareStatement(sql);
-		  pstmt.setString(1, login.getUserId());
-		  pstmt.setString(2, login.getPass());
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, login.getUserId());
+			pstmt.setString(2, login.getPass());
 
-		  ResultSet rs = pstmt.executeQuery();
+			ResultSet rs = pstmt.executeQuery();
 
-		  if(rs.next()) {
-			  String userId = rs.getString("USER_ID");
-			  String pass = rs.getString("PASS");
-			  String mail = rs.getString("MAIL");
-			  String name = rs.getString("NAME");
-			  user = new User(userId,pass,mail,name);
-		  }
-	  }catch(SQLException e) {
-		  e.printStackTrace();
-		  return null;
-	  }catch(ClassNotFoundException e) {
-		  e.printStackTrace();
-		  return null;
-	  }
-	  return user;
-  }
+			if (rs.next()) {
+				String userId = rs.getString("USER_ID");
+				String pass = rs.getString("PASS");
+				String mail = rs.getString("MAIL");
+				String name = rs.getString("NAME");
+				user = new User(userId, pass, mail, name);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return user;
+	}
+
+	public boolean RegisterUser(User user) {
+		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+			Class.forName(DB_DRIVER);
+
+			String sql = "INSERT INTO APPUSER (USER_ID, PASS, MAIL, NAME) VALUES(?,?,?,?)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getUserId());
+			pstmt.setString(2, user.getPass());
+			pstmt.setString(3, user.getMail());
+			pstmt.setString(4, user.getName());
+
+			int result = pstmt.executeUpdate();
+
+			if (result != 1) {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+
+	}
 }
