50c6e1b7c71f98e88b5d58405a2967f4273f1983
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-06 21:22:25 +0900

    コメントの変更

--- src/main/java/dao/RegisterTestDAO.java
+++ src/main/java/dao/RegisterTestDAO.java
@@ -7,13 +7,14 @@ import java.sql.SQLException;
 
 import model.User;
 
-//ユーザー登録テストで登録されたテストユーザーを削除するDAO
+
 public class RegisterTestDAO {
 	private final String JDBC_URL = "jdbc:mysql://localhost:3306/sampleappdb?characterEncording=UTF-8";
 	private final String DB_USER = "Sampleuser";
 	private final String DB_PASS = "chikuwanoowari458";
 	private final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 
+	//登録テストで登録されたテストユーザーを削除する
 	public boolean DeleteUser(User user) {
 		try (Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
 			Class.forName(DB_DRIVER);
