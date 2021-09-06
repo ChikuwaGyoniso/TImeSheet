aba463a206b76e901227d985b65058f58b7dba72
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-06 20:38:35 +0900

    UserId要素の追加

--- src/main/java/model/WorkTime.java
+++ src/main/java/model/WorkTime.java
@@ -3,7 +3,7 @@ package model;
 import java.sql.Time;
 
 public class WorkTime {
-
+	private String UserId;
 	private String Date;
 	private Time Start_Time;
 	private Time End_Time;
@@ -14,8 +14,10 @@ public class WorkTime {
 	private Time Holiday_Midnight_Time;
 	private Time Sum_WorkTime;
 
-	public WorkTime(String Date, Time Start_Time, Time End_Time, String Work_Contents, Time Nomal_Time, Time Midnight_Time,
+	public WorkTime(String UserId, String Date, Time Start_Time, Time End_Time, String Work_Contents, Time Nomal_Time,
+			Time Midnight_Time,
 			Time Holiday_Time, Time Holiday_Midnight_Time, Time Sum_WorkTime) {
+		this.UserId = UserId;
 		this.Date = Date;
 		this.Start_Time = Start_Time;
 		this.End_Time = End_Time;
@@ -26,40 +28,53 @@ public class WorkTime {
 		this.Holiday_Midnight_Time = Holiday_Midnight_Time;
 		this.Sum_WorkTime = Sum_WorkTime;
 	}
-   //日付
+
+	public String getUserId() {
+		return UserId;
+	}
+
+	//日付
 	public String getDate() {
 		return Date;
 	}
-  //業務開始時間
+
+	//業務開始時間
 	public Time getStart_Time() {
 		return Start_Time;
 
 	}
-  //業務終了時間
+
+	//業務終了時間
 	public Time getEnd_Time() {
 		return End_Time;
 	}
-  //仕事内容
+
+	//仕事内容
 	public String getWork_Contents() {
 		return Work_Contents;
 	}
-  //通常（休日および深夜の業務時間を除いた）時間
+
+	//通常（休日および深夜の業務時間を除いた）時間
 	public Time getNomal_Time() {
 		return Nomal_Time;
 	}
-  //深夜の労働時間
+
+	//深夜の労働時間
 	public Time getMidnight_Time() {
 		return Midnight_Time;
 	}
-  //休日の労働時間
+
+	//休日の労働時間
 	public Time getHoliday_Time() {
 		return Holiday_Time;
 	}
-  //休日深夜の労働時間
+
+	//休日深夜の労働時間
 	public Time getHoliday_Midnight_Time() {
 		return Holiday_Midnight_Time;
 	}
-  //労働時間の合計
+
+	//労働時間の合計
 	public Time getSum_WorkTime() {
 		return Sum_WorkTime;
 	}
