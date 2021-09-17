dabff624dfafe1cf701df1f7bca2c0e40bf51fc2
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-17 23:10:35 +0900

    String型をDate型に変換するクラスの作成

---
+++ src/main/java/model/ChangeStringDate.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+public class ChangeStringDate {
+	public static Date changeDate(String strDate) throws IllegalArgumentException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		java.util.Date date = new java.util.Date();
+
+		strDate = sdf.format(date);
+
+		Date date1 = Date.valueOf(strDate);
+
+		return date1;
+	}
+}
