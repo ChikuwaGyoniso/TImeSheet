66051600ee75066496cce694cd845acb0fe00988
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-09-30 22:01:51 +0900

    時間の書式処理の仕様変更

--- src/main/java/model/ChangeStringTime.java
+++ src/main/java/model/ChangeStringTime.java
@@ -9,10 +9,21 @@ public class ChangeStringTime {
 	public Time ChangeTime(String timeString) {
 
 		//DateTimeFormatterで入力された時間の書式を設定
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("kk:mm");
+
+		//入力された時間の書式を変更
+		String[] timearr = timeString.split(":");
+
+		if(timearr[0].length() == 1) {
+			timearr[0] = "0" + timearr[0];
+		}
+
+		if(timearr[1].length() == 1) {
+			timearr[1] = "0" + timearr[1];
+		}
 
 		//String型をLocalTime型に変更
-		LocalTime lt = LocalTime.parse(timeString, dtf);
+		LocalTime lt = LocalTime.parse(timearr[0] + ":" + timearr[1], dtf);
 
 		//LocalTime型をTime型に変更
 		Time time = Time.valueOf(lt);
