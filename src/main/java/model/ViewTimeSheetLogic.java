c28b3a17466b9a7e38d038f6431be5a5bd125fd0
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-10-02 11:41:14 +0900

    引数の追加

--- src/main/java/model/ViewTimeSheetLogic.java
+++ src/main/java/model/ViewTimeSheetLogic.java
@@ -5,9 +5,9 @@ import java.util.List;
 import dao.ViewTimeSheetDAO;
 
 public class ViewTimeSheetLogic {
-	public List<WorkTime> execute(User user) {
+	public List<WorkTime> execute(User user, YearAndMonth ymd) {
 		ViewTimeSheetDAO dao = new ViewTimeSheetDAO();
-		List<WorkTime> worktimelist = dao.findAll(user);
+		List<WorkTime> worktimelist = dao.findAll(user, ymd);
 		return worktimelist;
 	}
 }
