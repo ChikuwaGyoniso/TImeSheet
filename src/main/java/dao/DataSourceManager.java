b282095f8032c7847e953810e06d8fe90bcfa817
ChikuwaGyoniso <chikuwa.ishidaryu@docomo.ne.jp>
2021-11-20 16:25:19 +0900

    コネクションプールを作成する為のクラスの作成

---
+++ src/main/java/dao/DataSourceManager.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceManager {
+	private final String JNDI = "java:comp/env/jdbc/mysql";
+	protected Connection connection;
+
+	public DataSourceManager() {
+		try {
+			Context context = new InitialContext();
+			DataSource dataSource = (DataSource) context.lookup(JNDI);
+			connection = dataSource.getConnection();
+			connection.setAutoCommit(false);
+
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
